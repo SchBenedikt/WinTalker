7c56c9eca4b66d04aa53d8d1032d9142e867145c
Benedikt Schächner <benedikt.schaechner@web.de>
2023-08-04 20:36:17 +0200

    Delete open_word.py

--- open_word.py
+++
@@ -1,8 +0,0 @@
-import os
-from tkinter import messagebox
-
-def open_microsoft_word():
-    try:
-        os.system("start winword")  # Command to open Microsoft Word
-    except Exception as e:
-        messagebox.showerror("Error", f"Error opening Microsoft Word: {e}")
